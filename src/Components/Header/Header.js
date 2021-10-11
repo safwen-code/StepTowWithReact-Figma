12adea8356da042eec548dcd10f28fabc689dd91
safwen-code <safwendjebbi1234@gmail.com>
2021-10-11 23:14:38 +0100

    use styled components inn Link of react-router-dom

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,23 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: #5843E4;`}
+`
 function Header() {
   return (
     <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey/42">Survey</Link>
+      <StyledLink to="/">Accueil</StyledLink>
+      <StyledLink to="/survey/1" $isFullLink>
+        Survey
+      </StyledLink>
+      <StyledLink to="/freelances">freelances</StyledLink>
     </nav>
   )
 }
