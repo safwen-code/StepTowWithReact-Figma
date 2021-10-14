9b544ba75071302d753714ad85df9d8e3ec806be
safwen-code <safwendjebbi1234@gmail.com>
2021-10-15 00:13:53 +0100

    create Context

--- src/index.js
+++ src/index.js
@@ -8,28 +8,34 @@ import Error from './Components/Error/Error'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Resultat from './Components/Resultat/Resultat'
 import Freelancers from './Components/Freelancers/Freelancers'
-
+import Footer from './pages/Footer'
+import ThemProvider from './context/ThemProvider'
+import GlobalStyle from './utils/GlobalStyle'
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/survey/:questionNumber">
-          <Survey />
-        </Route>
-        <Route path="/result">
-          <Resultat />
-        </Route>
-        <Route path="/freelances">
-          <Freelancers />
-        </Route>
-        <Route>
-          <Error />
-        </Route>
-      </Switch>
+      <ThemProvider>
+        <GlobalStyle />
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/survey/:questionNumber">
+            <Survey />
+          </Route>
+          <Route path="/result">
+            <Resultat />
+          </Route>
+          <Route path="/freelances">
+            <Freelancers />
+          </Route>
+          <Route>
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </ThemProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
